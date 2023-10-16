682d13176bf8182eb6e1292edfc658943992e7d7
Martin Matovu <matovius@proton.me>
2023-10-16 16:26:35 +0300

    Configure wysiwyg color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,10 +46,37 @@ export default {
 		typography: {
 			DEFAULT: {
 				css: {
-					color: 'rgb(0, 0, 0 / 0.8)',
-					a: {
-						color: '#4f16c9'
-					}
+					'--tw-prose-headings': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-lead': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-links': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-bold': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-counters': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-bullets': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-hr': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-quotes': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-quote-borders': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-captions': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-code': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-pre-code': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-pre-bg': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-th-borders': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-td-borders': 'rgb(0, 0, 0 / 0.8)',
+					'--tw-prose-invert-body': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-headings': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-lead': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-links': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-bold': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-counters': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-bullets': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-hr': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-quotes': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-quote-borders': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-captions': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-code': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-pre-code': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
+					'--tw-prose-invert-th-borders': 'rgb(255, 255, 255 / 0.8)',
+					'--tw-prose-invert-td-borders': 'rgb(255, 255, 255 / 0.8)'
 				}
 			}
 		}
