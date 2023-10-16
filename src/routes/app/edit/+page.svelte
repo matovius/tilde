ffd92612932686a8737e7882581781f96068d1bb
Martin Matovu <matovius@proton.me>
2023-10-16 11:08:32 +0300

    Add '/app/edit'

---
+++ src/routes/app/edit/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Editor from '$lib/components/editor/Editor.svelte';
+</script>
+
+<svelte:head>
+	<title>Tilde Editor</title>
+</svelte:head>
+
+<div class="w-full h-full flex justify-center items-center">
+	<Editor />
+</div>
