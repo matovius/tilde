b0f84cffb21a9f5101e22df73f520fba2e2520f6
Martin Matovu <matovius@proton.me>
2023-10-29 16:28:29 +0300

    Implement export dialog plus other minor tweaks

--- src/lib/components/editor/Editor.svelte
+++ src/lib/components/editor/Editor.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { onMount, onDestroy, createEventDispatcher } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import { Editor, type JSONContent } from '@tiptap/core';
 	import StarterKit from '@tiptap/starter-kit';
+
 	import {
 		Bold,
 		Code,
-		CornerUpRight,
+		Download,
 		Heading1,
 		Heading2,
 		Heading3,
-		Info,
 		Italic,
 		List,
 		ListOrdered,
@@ -17,29 +17,16 @@
 		Quote,
 		Strikethrough,
 		Trash,
+		Trash2,
 		X
 	} from 'lucide-svelte';
+
 	import TildeLogo from '../TildeLogo.svelte';
 
 	let editorInterface: HTMLElement;
 	let editor: Editor;
 	let EditorContentHTML: string;
 	let EditorContentJSON: JSONContent;
-
-	let guideDialog: HTMLDialogElement;
-	let guideDialogOpen: boolean = false;
-
-	let clipboard: Clipboard;
-
-	function toggleGuideDialog() {
-		if (!guideDialogOpen) {
-			guideDialogOpen = true;
-			guideDialog.showModal();
-		} else {
-			guideDialog.close();
-			guideDialogOpen = false;
-		}
-	}
 
 	let discardDialog: HTMLDialogElement;
 	let discardDialogOpen: boolean = false;
@@ -84,7 +71,7 @@
 			editorProps: {
 				attributes: {
 					class:
-						'wysiwyg dark:wysiwyg-invert wysiwyg-sm tablet:wysiwyg tablet:dark:wysiwyg-invert w-full max-w-none h-full m-0 outline-none selection:bg-chrysler-500/20 dark:selection:bg-chrysler-500'
+						'wysiwyg dark:wysiwyg-invert wysiwyg-sm tablet:wysiwyg tablet:dark:wysiwyg-invert w-full max-w-none h-full m-0 outline-none selection:bg-chrysler/40 dark:selection:bg-chrysler'
 				}
 			},
 			element: editorInterface,
@@ -124,9 +111,9 @@
 		class="w-full h-full flex flex-col tablet:flex-row tablet:justify-center tablet:items-center overflow-hidden"
 	>
 		<header
-			class="w-full h-fit tablet:w-fit tablet:h-full p-3 flex flex-row tablet:flex-col justify-start items-center gap-3 sticky top-0"
+			class="w-full h-fit tablet:w-fit tablet:h-full flex flex-row tablet:flex-col justify-start items-center sticky top-0"
 		>
-			<div class="w-fit flex flex-row justify-center items-center gap-3">
+			<div class="w-fit flex flex-row justify-center items-center gap-3 p-3">
 				<div class="w-9 h-9" aria-label="Tilde logo">
 					<TildeLogo />
 				</div>
@@ -136,10 +123,10 @@
 			{#if editor}
 				<div
 					id="editor-controls"
-					class="w-full h-full p-2 flex flex-row tablet:flex-col justify-start items-center laptop:items-start gap-1 overflow-y-hidden tablet:overflow-y-auto overflow-x-auto tablet:overflow-x-hidden"
+					class="w-full h-full p-3 flex flex-row tablet:flex-col justify-start items-center laptop:items-start gap-1 overflow-y-hidden tablet:overflow-y-auto overflow-x-auto tablet:overflow-x-hidden"
 				>
 					<button
-						class="button"
+						class="button laptop:w-full"
 						on:click={() => editor.chain().focus().setParagraph().run()}
 						class:active={editor.isActive('paragraph')}
 					>
@@ -147,7 +134,7 @@
 						<span class="sr-only laptop:not-sr-only">Paragraph</span>
 					</button>
 					<button
-						class="button"
+						class="button laptop:w-full"
 						on:click={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
 						class:active={editor.isActive('heading', { level: 1 })}
 					>
@@ -155,7 +142,7 @@
 						<span class="sr-only laptop:not-sr-only">Heading 1</span>
 					</button>
 					<button
-						class="button"
+						class="button laptop:w-full"
 						on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
 						class:active={editor.isActive('heading', { level: 2 })}
 					>
@@ -163,7 +150,7 @@
 						<span class="sr-only laptop:not-sr-only">Heading 2</span>
 					</button>
 					<button
-						class="button"
+						class="button laptop:w-full"
 						on:click={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
 						class:active={editor.isActive('heading', { level: 3 })}
 					>
@@ -171,10 +158,8 @@
 						<span class="sr-only laptop:not-sr-only">Heading 3</span>
 					</button>
 
-					<hr class="w-px h-full tablet:w-full tablet:h-px text-black/5 dark:text-white/5" />
-
 					<button
-						class="button"
+						class="button laptop:w-full"
 						on:click={() => editor.chain().focus().toggleBold().run()}
 						class:active={editor.isActive('bold')}
 					>
@@ -182,7 +167,7 @@
 						<span class="sr-only laptop:not-sr-only">Bold</span>
 					</button>
 					<button
-						class="button"
+						class="button w-full"
 						on:click={() => editor.chain().focus().toggleItalic().run()}
 						class:active={editor.isActive('italic')}
 					>
@@ -190,27 +175,31 @@
 						<span class="sr-only laptop:not-sr-only">Italic</span>
 					</button>
 					<button
-						class="button"
+						class="button w-full"
 						on:click={() => editor.chain().focus().toggleStrike().run()}
 						class:active={editor.isActive('strike')}
 					>
 						<Strikethrough />
 						<span class="sr-only laptop:not-sr-only">Strikethrough</span>
 					</button>
-					<button class="button">
+					<button
+						class="button w-full"
+						on:click={() => editor.chain().focus().toggleBlockquote().run()}
+						class:active={editor.isActive('blockquote')}
+					>
 						<Quote />
-						<span class="sr-only laptop:not-sr-only">Quote</span>
+						<span class="sr-only laptop:not-sr-only">Blockquote</span>
 					</button>
 					<button
-						class="button"
-						on:click={() => editor.chain().focus().toggleCode().run()}
-						class:active={editor.isActive('code')}
+						class="button w-full"
+						on:click={() => editor.chain().focus().toggleCodeBlock().run()}
+						class:active={editor.isActive('codeBlock')}
 					>
 						<Code />
-						<span class="sr-only laptop:not-sr-only">Code</span>
+						<span class="sr-only laptop:not-sr-only">Codeblock</span>
 					</button>
 					<button
-						class="button"
+						class="button w-full"
 						on:click={() => editor.chain().focus().toggleBulletList().run()}
 						class:active={editor.isActive('bulletList')}
 					>
@@ -218,7 +207,7 @@
 						<span class="sr-only laptop:not-sr-only">Bullet List</span>
 					</button>
 					<button
-						class="button"
+						class="button w-full"
 						on:click={() => editor.chain().focus().toggleOrderedList().run()}
 						class:active={editor.isActive('orderedList')}
 					>
@@ -232,7 +221,7 @@
 		<main class="w-full h-full flex flex-col justify-center items-start overflow-hidden">
 			<div
 				id="editor-interface"
-				class="w-full h-full p-6 flex justify-center items-center overflow-y-auto bg-black/5 dark:bg-white/5"
+				class="w-full h-full p-6 flex justify-center items-center overflow-y-auto border-y tablet:border-y-0 tablet:border-x border-black/10 dark:border-white/10"
 				bind:this={editorInterface}
 			/>
 		</main>
@@ -243,33 +232,50 @@
 			{#if editor}
 				<div
 					id="more-options"
-					class="w-full tablet:h-full flex flex-row tablet:flex-col justify-start items-center laptop:items-start gap-1"
+					class="w-full tablet:h-full flex flex-row tablet:flex-col justify-between items-center laptop:items-start gap-1"
 				>
-					<div class="hidden">
-						<button class="button" on:click={toggleGuideDialog}>
-							<Info />
-							<span class="sr-only laptop:not-sr-only">Guide</span>
+					<div>
+						<button class="button" on:click={toggleExportDialog}>
+							<Download />
+							<span class="sr-only laptop:not-sr-only">Export</span>
 						</button>
 						<dialog
-							bind:this={guideDialog}
+							bind:this={exportDialog}
 							class="w-full max-w-lg p-3 bg-transparent backdrop:bg-black/50 backdrop:backdrop-blur"
 						>
 							<div
-								class="w-full min-h-[320px] p-3 rounded-3xl text-black/80 dark:text-white/80 bg-white dark:bg-black relative"
+								class="w-full p-3 rounded-3xl text-black/80 dark:text-white/80 bg-white dark:bg-black relative"
 							>
 								<div class="pt-3 pr-3 absolute top-0 right-0">
-									<button class="button" on:click={toggleGuideDialog}>
+									<button class="button" on:click={toggleExportDialog}>
 										<X />
 										<span class="sr-only">Close</span>
 									</button>
 								</div>
+								<header class="w-full p-3">
+									<h2 class="text-xl">Export</h2>
+								</header>
+								<main class="w-full p-3 flex flex-col gap-3">
+									<p>You can currently get your content as either JSON or HTML.</p>
+									<p>
+										There will eventually be support for Markdown, so be on the look-out for that.
+									</p>
+								</main>
+								<footer class="w-full p-3 flex flex-col tablet:flex-row tablet:justify-end gap-1">
+									<button class="button primary">
+										<span>Download JSON</span>
+									</button>
+									<button class="button primary">
+										<span>Download HTML</span>
+									</button>
+								</footer>
 							</div>
 						</dialog>
 					</div>
 
 					<div>
-						<button class="button" on:click={toggleDiscardDialog}>
-							<Trash />
+						<button class="button danger" on:click={toggleDiscardDialog}>
+							<Trash2 />
 							<span class="sr-only laptop:not-sr-only">Discard</span>
 						</button>
 						<dialog
@@ -297,45 +303,6 @@
 									<button class="button primary" on:click={discardEditorContent}
 										>Yes, discard</button
 									>
-								</footer>
-							</div>
-						</dialog>
-					</div>
-
-					<div>
-						<button class="button" on:click={toggleExportDialog}>
-							<CornerUpRight />
-							<span class="sr-only laptop:not-sr-only">Export</span>
-						</button>
-						<dialog
-							bind:this={exportDialog}
-							class="w-full max-w-lg p-3 bg-transparent backdrop:bg-black/50 backdrop:backdrop-blur"
-						>
-							<div
-								class="w-full p-3 rounded-3xl text-black/80 dark:text-white/80 bg-white dark:bg-black relative"
-							>
-								<div class="pt-3 pr-3 absolute top-0 right-0">
-									<button class="button" on:click={toggleExportDialog}>
-										<X />
-										<span class="sr-only">Close</span>
-									</button>
-								</div>
-								<header class="w-full p-3">
-									<h2 class="text-xl">Export</h2>
-								</header>
-								<main class="w-full p-3 flex flex-col gap-3">
-									<p>You can currently get your content as either JSON or HTML.</p>
-									<p>
-										There will eventually be support for Markdown, so be on the look-out for that.
-									</p>
-								</main>
-								<footer class="w-full p-3 flex flex-col tablet:flex-row tablet:justify-end gap-1">
-									<button class="button primary">
-										<span>Copy JSON</span>
-									</button>
-									<button class="button primary">
-										<span>Copy HTML</span>
-									</button>
 								</footer>
 							</div>
 						</dialog>
