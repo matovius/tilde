f02cb511b1ca3b87437d9c7ea8dbaf552d735a00
Martin Matovu <matovius@proton.me>
2023-10-30 02:06:54 +0300

    Edit page structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,26 +7,41 @@
 	<title>Tilde: Your favorite online text editor</title>
 </svelte:head>
 
-<div class="w-full h-full overflow-y-auto px-6 flex flex-col justify-between items-center">
+<div class="w-full h-full overflow-y-auto flex flex-col justify-start items-center">
+	<header class="w-full max-w-[52.5rem] p-24">
+		<nav class="w-full">
+			<ul class="w-full flex flex-row justify-center items-center gap-24">
+				<li class="link">
+					<a href="/why-tilde" class="link hover:text-white"> Why Tilde? </a>
+				</li>
+				<li class="link">
+					<a href="/availability" class="link hover:text-white"> Availability </a>
+				</li>
+			</ul>
+		</nav>
+	</header>
 	<main
-		class="w-full max-w-[720px] flex flex-col justify-start items-center gap-8 tablet:gap-16 pt-6 tablet:pt-[120px]"
+		class="w-full max-w-[52.5rem] h-full flex flex-col justify-start items-center gap-24 tablet:gap-48 py-24 tablet:py-48 px-24"
 	>
-		<div class="w-24 h-24 aspect-square">
+		<div class="w-96 h-96 aspect-square">
 			<TildeLogo />
 		</div>
 		<h1 class="text-center text-6xl font-bold">Welcome to Tilde</h1>
 		<h2 class="text-center text-4xl font-medium text-black/60 dark:text-white/60">
-			Your favorite web-based text editor
+			The minimal web-based text editor
 		</h2>
-		<a href="/app" class="button text-white bg-chrysler hover:bg-chrysler/80 focus:bg-chrysler/80">
+		<a
+			href="/dashboard"
+			class="button text-white bg-chrysler hover:bg-chrysler/80 focus:bg-chrysler/80"
+		>
 			<span>Get Started</span>
 			<ArrowRight />
 		</a>
 	</main>
-	<footer class="w-full max-w-[720px] py-6 flex justify-center items-center">
+	<footer class="w-full max-w-[52.5rem] p-24 flex justify-center items-center">
 		<span class="text-center text-xs font-medium">
 			Tilde is currently only available as a demo. Read more about what this means
-			<a href="/availability" class="link text-chrysler-500">here</a>.
+			<a href="/availability" class="link text-chrysler">here</a>.
 		</span>
 	</footer>
 </div>
