c58ba6ecf0167c70e7f56648fa0e73c458761cb3
Martin Matovu <matovius@proton.me>
2023-10-29 16:30:51 +0300

    Remove unnecessary exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,3 @@
-// place files you want to import through the `$lib` alias in this folder.
-export * from './components/Tiptap.svelte';
 export * from './components/TildeLogo.svelte';
 export * from './components/app/BottomNav.svelte';
 export * from './components/app/Sidebar.svelte';
