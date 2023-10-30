981b9d17317b32d16844289fd470c84e5331e6b4
Martin Matovu <matovius@proton.me>
2023-10-30 16:00:41 +0300

    Add experimental editor

---
+++ src/lib/components/editor/ProseEditor.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { EditorView } from 'codemirror';
+	import { markdown } from '@codemirror/lang-markdown';
+	import { syntaxHighlighting } from '@codemirror/language';
+	import { onMount } from 'svelte';
+
+	let editorParent: HTMLElement;
+
+	let theme = EditorView.baseTheme({});
+
+	onMount(() => {
+		let editor = new EditorView({
+			extensions: [markdown()],
+			parent: editorParent
+		});
+	});
+</script>
+
+<main class="w-full h-full flex flex-col justify-center items-start overflow-hidden">
+	<div
+		id="editor-interface"
+		class="w-full h-full p-6 flex justify-start items-start overflow-y-auto bg-black/5 dark:bg-white/5"
+		bind:this={editorParent}
+	/>
+</main>
