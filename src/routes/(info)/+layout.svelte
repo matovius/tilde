3573582ecf40923a1fb2ee6d64e266331252f8fd
Martin Matovu <matovius@proton.me>
2023-10-30 20:58:09 +0300

    Revamp layout

--- src/routes/(info)/+layout.svelte
+++ src/routes/(info)/+layout.svelte
@@ -1,23 +1,35 @@
 <script lang="ts">
 	import TildeLogo from '$lib/components/TildeLogo.svelte';
+	import { ArrowLeft } from 'lucide-svelte';
+
+	function goToPreviousPage(): void {
+		window.history.back();
+	}
 </script>
 
-<div class="w-full h-full overflow-y-auto flex justify-center items-start">
-	<div class="w-full max-w-[640px] h-full p-6 flex flex-col">
-		<header class="w-full flex justify-center items-center not-wysiwyg py-6 laptop:py-12">
-			<div class="w-24 laptop:w-24 h-12 laptop:h-24 aspect-square">
-				<TildeLogo />
-			</div>
-		</header>
+<header class="w-full flex justify-center items-center">
+	<div class="w-full max-w-[52.5rem] p-24 flex justify-center items-center">
+		<div class="w-60 h-60 aspect-square">
+			<TildeLogo />
+		</div>
+	</div>
+</header>
 
-		<main
-			class="w-full max-w-none h-full overflow-y-auto wysiwyg dark:wysiwyg-invert wysiwyg-sm tablet:wysiwyg tablet:dark:wysiwyg-invert selection:bg-chrysler-500/20 dark:selection:bg-chrysler-500"
+<main class="w-full flex justify-center items-center">
+	<div class="w-full max-w-[52.5rem] p-24 flex justify-center items-center">
+		<div
+			class="max-w-none wysiwyg dark:wysiwyg-invert wysiwyg-sm tablet:wysiwyg tablet:dark:wysiwyg-invert selection:bg-chrysler/20 dark:selection:bg-chrysler"
 		>
 			<slot />
-		</main>
-
-		<footer class="w-full flex justify-start items-start py-2">
-			<a href="/" class="not-wysiwyg link text-chrysler-500 dark:text-chrysler-200">Go Back</a>
-		</footer>
+		</div>
 	</div>
-</div>
+</main>
+
+<footer class="w-full flex justify-center items-center">
+	<div class="w-full max-w-[52.5rem] p-24">
+		<button class="button" on:click={goToPreviousPage}>
+			<ArrowLeft />
+			<span>Go Back</span>
+		</button>
+	</div>
+</footer>
