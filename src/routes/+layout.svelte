6932ce8c81f9009a7d9dd1a8d2406f6e005f208e
Martin Matovu <matovius@proton.me>
2023-10-30 16:00:07 +0300

    Add auto overflow on y-axis

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 	import '@fontsource-variable/fira-code';
 </script>
 
-<div id="root" class="text-black/80 dark:text-white/80 h-[100dvh] bg-white dark:bg-black/90">
+<div
+	id="root"
+	class="text-black/80 dark:text-white/80 h-[100dvh] bg-white dark:bg-black/90 overflow-y-auto"
+>
 	<slot />
 </div>
