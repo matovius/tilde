3f5be36a189ed0934e0cddaa95470853d82d93e3
Martin Matovu <matovius@proton.me>
2023-10-30 20:19:30 +0300

    Add logic for sign in button activation

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -2,6 +2,18 @@
 	import { Eye, EyeOff } from 'lucide-svelte';
 
 	let passwordVisible: boolean = false;
+
+	let EmailInput: HTMLInputElement;
+	let PasswordInput: HTMLInputElement;
+	let SignInButton: HTMLButtonElement;
+
+	function handleSIgnInButtonActivity() {
+		if (EmailInput.value !== '' && PasswordInput.value !== '') {
+			SignInButton.removeAttribute('disabled');
+		} else {
+			SignInButton.setAttribute('disabled', '');
+		}
+	}
 </script>
 
 <svelte:head>
@@ -9,7 +21,7 @@
 </svelte:head>
 
 <div id="heading" class="w-full flex justify-center items-center text-center">
-	<h1 class="h1">Sign Into Tilde</h1>
+	<h1 class="h1">Sign In</h1>
 </div>
 
 <form
@@ -25,16 +37,31 @@
 			id="email-address"
 			placeholder="janedoe@example.com"
 			class="input"
+			bind:this={EmailInput}
+			on:input={handleSIgnInButtonActivity}
 		/>
 	</label>
 	<label for="password" class="w-full flex flex-col gap-8 relative">
 		<span>Password</span>
 		<div class="w-full flex flex-row gap-8">
-			<input type="password" name="password" id="password" class="input" />
+			<input
+				type="password"
+				name="password"
+				id="password"
+				class="input"
+				bind:this={PasswordInput}
+				on:input={handleSIgnInButtonActivity}
+			/>
 			<button
 				class="button"
 				on:click={() => {
 					passwordVisible = !passwordVisible;
+
+					if (passwordVisible) {
+						PasswordInput.type = 'text';
+					} else {
+						PasswordInput.type = 'password';
+					}
 				}}
 			>
 				{#if passwordVisible}
@@ -48,7 +75,11 @@
 		</div>
 	</label>
 	<div class="w-full">
-		<button class="button primary w-full justify-center items-center" disabled>Sign In</button>
+		<button
+			class="button primary w-full justify-center items-center"
+			disabled
+			bind:this={SignInButton}>Sign In</button
+		>
 	</div>
 </form>
 
@@ -56,6 +87,6 @@
 	<h2 class="h2">Or</h2>
 
 	<div class="w-full">
-		<button class="button outline w-full justify-center items-center">SIgn In as Guest</button>
+		<button class="button outline w-full justify-center items-center">Sign In as Guest</button>
 	</div>
 </div>
