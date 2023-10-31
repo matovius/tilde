99b5692bb2f9d3681687bcb1697c091480d2bed2
Martin Matovu <matovius@proton.me>
2023-11-01 02:00:24 +0300

    Revamp page content

--- src/routes/(info)/availability/+page.svelte
+++ src/routes/(info)/availability/+page.svelte
@@ -5,19 +5,23 @@
 	<title>Tilde's Availability</title>
 </svelte:head>
 
-<div class="w-full">
-	<h1 class="text-center">Tilde's Availability</h1>
+<h1 class="text-center">Tilde's Availability</h1>
 
-	<p>
-		As mentioned on the landing page, Tilde is currently only available as a demo. This doesn't mean
-		it has no functionality. All visible elements are functional.
-	</p>
-	<p>
-		What this means is that anything you write in the editor is not saved on a server. It is
-		however, saved to your browser's <code>localStorage</code>.
-	</p>
-	<p>
-		There will eventually be a way for you to share what you write via a temporary URL that's only
-		available for 24-hours until renewed by visiting that URL within 23 hours after creation.
-	</p>
-</div>
+<p>
+	Tilde has only one account that can be signed into, that of me the author,
+	<a href="http://github.com/matovius" target="_blank" rel="noopener noreferrer" class="link"
+		>Martin</a
+	>. You can use a guest account to sign in and use the editor as well, but you won't be able to use
+	any of the features that require the use of a server, including saving your content. In the
+	future, I might add a feature where you can share your writing as a URL, but for now all you'll be
+	able to do is download a file of either HTML, JSON, or Markdown formats, which is probably plenty
+	for what you might need out of this editor.
+</p>
+<p>
+	If you happen to need more out of this editor, I advise you to go and fork my repo
+	<a href="https://github.com/matovius/tilde" target="_blank" rel="noopener noreferrer" class="link"
+		>here</a
+	>, set up your own instance with your own account, and use however you want. I'm still working a
+	lot on this project, and a lot more new features you will only enjoy with your own instance and
+	account.
+</p>
