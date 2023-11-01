56a6cc9d93b63eb7ee27cd680df4daaaca36ac7e
Martin Matovu <matovius@proton.me>
2023-11-01 03:05:57 +0300

    Remove unnecessary packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { preprocessMeltUI } from '@melt-ui/pp';
-import sequence from 'svelte-sequential-preprocessor';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +12,7 @@ const config = {
 
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: sequence([vitePreprocess(), preprocessMeltUI()])
+	preprocess: vitePreprocess()
 };
 
 export default config;
