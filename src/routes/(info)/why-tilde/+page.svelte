49ece6fe02a994d449d0ea8ddbc0168480d6ef2a
Martin Matovu <matovius@proton.me>
2023-11-01 03:19:15 +0300

    Edit page content and rendering mode

--- src/routes/(info)/why-tilde/+page.svelte
+++ src/routes/(info)/why-tilde/+page.svelte
@@ -1,1 +1,10 @@
-<div>Why Tilde</div>
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Why I Made Tilde</title>
+</svelte:head>
+
+<h1 class="text-center">Why Tilde?</h1>
+
+<p>This is why I made Tilde.</p>
