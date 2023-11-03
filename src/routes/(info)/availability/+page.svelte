2bbb32e38a285c21e7c989987f28c1a42602f108
Martin Matovu <matovius@proton.me>
2023-11-03 19:37:36 +0300

    Change page content

--- src/routes/(info)/availability/+page.svelte
+++ src/routes/(info)/availability/+page.svelte
@@ -8,20 +8,13 @@
 <h1 class="text-center">Tilde's Availability</h1>
 
 <p>
-	Tilde has only one account that can be signed into, that of me the author,
-	<a href="http://github.com/matovius" target="_blank" rel="noopener noreferrer" class="link"
-		>Martin</a
-	>. You can use a guest account to sign in and use the editor as well, but you won't be able to use
-	any of the features that require the use of a server, including saving your content. In the
-	future, I might add a feature where you can share your writing as a URL, but for now all you'll be
-	able to do is download a file of either HTML, JSON, or Markdown formats, which is probably plenty
-	for what you might need out of this editor.
+	Tilde is only available as a demo. Anything and everything you write in the editor stays in the
+	editor stays in your browser. You can download this content if you need it. There's currently two
+	download options: HTML and JSON. Markdown is currently in the works and will probably be avaliable
+	some time soon.
 </p>
 <p>
-	If you happen to need more out of this editor, I advise you to go and fork my repo
-	<a href="https://github.com/matovius/tilde" target="_blank" rel="noopener noreferrer" class="link"
-		>here</a
-	>, set up your own instance with your own account, and use however you want. I'm still working a
-	lot on this project, and a lot more new features you will only enjoy with your own instance and
-	account.
+	All your data will be erased once you close the browser tab or window unless you download it. Your
+	data will at some point be saved in <code>localStorage</code> to help you not lose any of your data
+	in case something weird happens.
 </p>
