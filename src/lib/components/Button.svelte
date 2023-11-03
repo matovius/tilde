91f53e89f240d4ea4472287a1afdd916d3a06a60
Martin Matovu <matovius@proton.me>
2023-11-03 18:13:24 +0300

    Add dedicated button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let as: 'button' | 'link';
+	export let style: 'btn-solid' | 'btn-outline' | 'btn-ghost' = 'btn-ghost';
+	export let variant: 'btn-primary' | 'btn-neutral' | 'btn-danger' = 'btn-neutral';
+	export let padX: 'x-4' | 'x-8' | 'x-12' | 'x-24' | 'x-36' = 'x-12';
+	export let padY: 'y-4' | 'y-8' | 'y-12' | 'y-24' | 'y-36' = 'y-8';
+	export let width: 'fit' | 'full' = 'fit';
+	export let spacing: string = 'gap-12';
+	export let url: string = '/';
+</script>
+
+{#if as === 'button'}
+	<button class={`btn ${style} ${variant} ${padX} ${padY} ${width} ${spacing}`} on:click>
+		<slot />
+	</button>
+{:else if as === 'link'}
+	<a href={url} class={`btn ${style} ${variant} ${padX} ${padY} ${width} ${spacing}`} on:click>
+		<slot />
+	</a>
+{/if}
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export * from './components/TildeLogo.svelte';
 export * from './components/app/BottomNav.svelte';
 export * from './components/app/Sidebar.svelte';
 export * from './components/editor/Editor.svelte';
+export * from './components/Button.svelte';
